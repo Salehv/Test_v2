242155de5a7afe66159372fb348db75e6bbc969d
Salehv <salehv@gmail.com>
2019-06-24 18:35:25 +0430

    Version 1.1.1.44

--- Assets/Scripts/App/GameViewManager.cs
+++ Assets/Scripts/App/GameViewManager.cs
@@ -9,43 +9,38 @@ namespace App
         internal static GameViewManager instance;
         private ViewManager view;
         public GameObject gamePanelsObject;
-    
-        [Header("Panels")] 
-        [SerializeField] private Panel winPanel;
+
+        [Header("Panels")] [SerializeField] private Panel winPanel;
         [SerializeField] private Panel pausePanel;
         [SerializeField] private Panel hintPanel;
         [SerializeField] private Panel hintSimilarWordsPanel;
 
-        
-    
-        [Header("Game View")]
-        public Image inGameBack;
+
+        [Header("Game View")] public Image inGameBack;
         public Image inGameFeatures;
 
 
-        [Header("Objects")] 
-        public Transform wordsView;
+        [Header("Objects")] public Transform wordsView;
         public GameObject acceptedWordPrefab;
-    
+
         public Text hintSimilarCostText;
         public Text hintWayCostText;
 
-        [Header("UI Handlers")]
-        public WinPanelHandler winHandler;
+        [Header("UI Handlers")] public WinPanelHandler winHandler;
         public EndWordHandler endWordHandler;
 
 
         private GameViewState state;
-        
+
         private void Awake()
         {
             instance = this;
             view = ViewManager.instance;
         }
 
-        
+
         #region Initialize
-        
+
         public void ShowGame()
         {
             gamePanelsObject.SetActive(true);
@@ -57,14 +52,14 @@ namespace App
             state = GameViewState.MAIN_VIEW;
         }
 
-        
+
         public void SetInGameGraphics(int chapter)
         {
             inGameBack.sprite = ResourceManager.GetInGameBackground(chapter);
             inGameFeatures.sprite = ResourceManager.GetInGameFeatures(chapter);
         }
 
-        
+
         public void SetEndWord(string end)
         {
             for (int i = 0; i < endWordHandler.contents.childCount; i++)
@@ -85,35 +80,35 @@ namespace App
 
         #endregion
 
-        
+
         #region WordsView
 
         public void ClearWordsView()
         {
-            for(int i = 0; i < wordsView.childCount; i++)
-                Destroy(wordsView.GetChild(i).gameObject);    
+            for (int i = 0; i < wordsView.childCount; i++)
+                Destroy(wordsView.GetChild(i).gameObject);
         }
 
         public void AddToWordsView(string word)
         {
             GameObject g = Instantiate(acceptedWordPrefab, wordsView);
             g.GetComponentInChildren<Text>().text = word;
-            
+
             StepViewerHandler.instance.StepForward();
         }
 
         public void RemoveLastWord()
         {
             Destroy(wordsView.GetChild(wordsView.childCount - 1).gameObject);
-            
+
             StepViewerHandler.instance.StepBackward();
         }
 
         #endregion
 
-        
+
         #region Panels
-        
+
         public void ShowWinPanel(int gem, int coinGain)
         {
             state = GameViewState.WIN;
@@ -125,7 +120,7 @@ namespace App
         {
             view.ShowPanel(hintPanel);
         }
-        
+
         public void ShowSimilarWordsHintPanel()
         {
             view.ShowPanel(hintSimilarWordsPanel);
@@ -137,11 +132,10 @@ namespace App
             view.SetEscapable();
             view.Escape();
         }
-        
+
         #endregion
-        
-        
-        
+
+
         internal void Escape()
         {
             switch (state)
@@ -151,10 +145,7 @@ namespace App
                     break;
             }
         }
-        
     }
-    
-    
 }
 
 public enum GameViewState
