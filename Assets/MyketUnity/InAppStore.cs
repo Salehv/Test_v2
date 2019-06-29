8e067da54bfd8413bbaddf83baa35830562a9ed1
Noobody14 <Noobody14@outlook.com>
2019-06-29 12:20:43 +0430

    Myket IAB

---
+++ Assets/MyketUnity/InAppStore.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(StoreHandler))]
+public class InAppStore : MonoBehaviour, IBillingListener
+{
+    public Product[] products;
+
+    void Awake()
+    {
+        GetComponent<StoreHandler>().SetUpBillingService(this);
+    }
+
+    void Start()
+    {
+    }
+
+
+    public void OnBillingServiceSetupFinished()
+    {
+        GetComponent<StoreHandler>().UpdatePurchasesAndDetails(products);
+    }
+
+
+    public void OnPurchasesUpdated(List<Purchase> purchases)
+    {
+    }
+
+    public void OnPurchasesAndDetailsUpdated(List<Purchase> purchases, List<ProductDetail> productDetails)
+    {
+    }
+
+    public void OnUserCancelPurchase(string errorMessage)
+    {
+    }
+
+    public void OnPurchaseFinished(Purchase purchase)
+    {
+    }
+
+    public void OnConsumeFinished(String productId, String purchaseToken)
+    {
+    }
+
+    public void OnError(Int32 errorCode, String errorMessage)
+    {
+        switch (errorCode)
+        {
+            case 1: // error illegal State
+                break;
+
+            case 2: // error billing setup fail
+                break;
+
+            case 3: // error empty public key
+                break;
+
+            case 4: // error update purchases
+
+                break;
+            case 5: // error consume
+
+                break;
+            case 6: // error start activity
+
+                break;
+            case 7: // error purchase
+
+                break;
+        }
+    }
+
+
+    public void purchaseProduct(int productIndex)
+    {
+        Product product = products[productIndex];
+        if (product.type == Product.ProductType.Subscription)
+        {
+            GetComponent<StoreHandler>().BuyProduct(product.productId, "subs");
+        }
+        else if (product.type == Product.ProductType.InApp)
+        {
+            GetComponent<StoreHandler>().BuyProduct(product.productId, "inapp");
+        }
+    }
+}
--- Assets/Scripts/IAB/IABHandler.cs
+++ Assets/Scripts/IAB/IABHandler.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using BazaarPlugin;
+﻿using System.Collections.Generic;
 using TheGame;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class IABHandler : MonoBehaviour
+public class IABHandler : MonoBehaviour, IBillingListener
 {
     [SerializeField] [Header("Shop items")]
     public bool debugMode;
+
     public IABItem[] items;
 
     [Header("UI Objects")] public GameObject shopLock;
+
+    public StoreHandler storeHandler;
+
+    public InAppStore inAppStore;
     // public GameObject shopLoading;
 
     // public Text debug;
@@ -29,94 +28,58 @@ public class IABHandler : MonoBehaviour
         {
             itemMap.Add(item.productId, item);
         }
-        
-        InitCafeBazaar();
+
+        InitMyket();
     }
 
-
-    public void InitCafeBazaar()
+    private void InitMyket()
     {
-        InitEvents();
-
-        BazaarIAB.init(
-            "MIHNMA0GCSqGSIb3DQEBAQUAA4G7ADCBtwKBrwCooaFh1ZJhgj1fHE6inFBGTKXIzeWYC7d6gdonZ1AxdAWZykxjbhIN1YbNpEc22hwz/A" +
-            "bQdxhMXO8jLRnUdbuCp/9k0BYQuPVkmCa0wo3jdthokV2jClTMrDhAj3rNBFQ+s49K0h2SN/iH7+AcTszoqSMGtsG5Hhm0eZYyWBHf6IUOU5E0Tex+ku" +
-            "9BaSf5LdEYh/QTXznV0GOhJ0MEzaLWRCPnZmzyaCvU7qeHk8ECAwEAAQ==");
-    }
-
-    private void InitEvents()
-    {
-        IABEventManager.billingSupportedEvent += BillingSupported;
-        IABEventManager.billingNotSupportedEvent += BillingNotSupported;
-        IABEventManager.querySkuDetailsSucceededEvent += SkuDetailsQuerySucceed;
-        IABEventManager.querySkuDetailsFailedEvent += SkuDetailQueryFailed;
-        IABEventManager.purchaseSucceededEvent += PurchaseSucceed;
-        IABEventManager.consumePurchaseSucceededEvent += consumeSucceed;
-    }
-
-
-    private void BillingSupported()
-    {
-        //shopLoading.SetActive(true);
-        Debug.Log("IAB Supported");
-    }
-
-    private void BillingNotSupported(string error)
-    {
-        Debug.LogError(error);
-        shopLock.SetActive(true);
-    }
-
-    private void UpdateShopItems()
-    {
-        string[] details = itemMap.Keys.ToArray();
-        BazaarIAB.querySkuDetails(details);
-    }
-
-
-    private void SkuDetailsQuerySucceed(List<BazaarSkuInfo> skus)
-    {
-        foreach (BazaarSkuInfo sku in skus)
-        {
-            itemMap[sku.ProductId].priceText.text = sku.Price;
-        }
-    }
-
-    private void SkuDetailQueryFailed(string error)
-    {
-        PopupHandler.ShowInfo("فکر کنم اینترنتت وصل نیست . . .", RetryConnection);
-    }
-
-    public void RetryConnection()
-    {
-        UpdateShopItems();
-    }
-
-
-    private void PurchaseSucceed(BazaarPurchase purchase)
-    {
-        BazaarIAB.consumeProduct(purchase.ProductId);
-    }
-
-    private void consumeSucceed(BazaarPurchase purchase)
-    {
-        PopupHandler.ShowDebug("خرید موفقیت آمیز بود");
-        GameManager.instance.AddCoins(itemMap[purchase.ProductId].amount);
-        AnalyticsHandler.ItemBought(itemMap[purchase.ProductId].type, itemMap[purchase.ProductId].price,
-            itemMap[purchase.ProductId].amount);
+        storeHandler.SetUpBillingService(this);
+        storeHandler.UpdatePurchases();
     }
 
 
     public void PurchaseItem(string id)
     {
-        if(debugMode)
+        if (debugMode)
             print($"[IABHandler] Purchase item {id}");
-        BazaarIAB.purchaseProduct(id);
+
+        inAppStore.purchaseProduct(int.Parse(id.Split('_')[1]));
     }
-}
 
+    public void OnBillingServiceSetupFinished()
+    {
+        print("[IABHandler] Setup Finished");
+    }
 
-enum IABState
-{
-    q
+    public void OnPurchasesUpdated(List<Purchase> purchases)
+    {
+        purchases.ForEach(OnPurchaseFinished);
+    }
+
+    public void OnPurchasesAndDetailsUpdated(List<Purchase> purchases, List<ProductDetail> products)
+    {
+    }
+
+    public void OnUserCancelPurchase(string errorMessage)
+    {
+    }
+
+    public void OnPurchaseFinished(Purchase purchase)
+    {
+        storeHandler.ConsumePurchase(purchase.productId, purchase.purchaseToken);
+    }
+
+    public void OnConsumeFinished(string productId, string purchaseToken)
+    {
+        PopupHandler.ShowDebug("خرید موفقیت آمیز بود");
+        GameManager.instance.AddCoins(itemMap[productId].amount);
+        AnalyticsHandler.ItemBought(itemMap[productId].type, itemMap[productId].price,
+            itemMap[productId].amount);
+    }
+
+    public void OnError(int errorCode, string errorMessage)
+    {
+        Debug.LogError("IAB ERROR");
+    }
 }
